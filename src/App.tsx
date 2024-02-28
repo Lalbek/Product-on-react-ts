7266d5367e8b48361e883987ba723eb27e00908c
Lalbek Sherbekov <sherbekovlalbek21@gmail.com>
2024-02-28 21:52:13 +0100

    task-4 made navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Router } from "../src/Router/Router";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
+      <Navbar />
     </BrowserRouter>
   );
 }
